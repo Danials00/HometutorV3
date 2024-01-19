67e1a16ed5d2ea829a2637ecfa5b279e697138ae
Danials00 <adzdanial95@gmail.com>
2024-01-19 15:29:42 +0800

    Update Build

--- src/COMPONENTS/appbar.tsx
+++ src/COMPONENTS/appbar.tsx
@@ -10,7 +10,7 @@ import Tooltip from '@mui/material/Tooltip';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import signin_logo from '../../public/signin_logo.svg';
+import signin_logo from '../../public/Logo_Xcelearn_main.png';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Stack from '@mui/material/Stack';
 import AppsIcon from '@mui/icons-material/Apps';
